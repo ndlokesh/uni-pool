6c7703725cc46c41c43c3929a44111993e396033
Uni Pool User <user@example.com>
2026-02-06 20:17:09 +0530

    Add Vercel Speed Insights

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
 
